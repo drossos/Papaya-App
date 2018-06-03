c6eb97fd374d3848f0eb00fed2f60579d7dd7078
Arun Jolly Marlier <ajollymarlier@bayviewglen.ca>
2018-06-03 14:48:51 -0400

    yee

---
+++ app/src/main/java/papaya/bayviewglen/com/papayatest/AddTags.java
@@ -0,0 +1,14 @@
+package papaya.bayviewglen.com.papayatest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTags extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
